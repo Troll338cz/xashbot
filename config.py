fcb008553888a27bf2e8a4509eaabefe998fe458
Troll338cz <59578676+Troll338cz@users.noreply.github.com>
2026-02-10 12:33:13 +0100

    Update file paths for names and chat configuration

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 CONFIG = {
     'connections': 5,
     'delay': 0.1,
-    'names_file': 'names.txt',
-    'chat_file': 'chat.txt',
+    'names_file': 'names/noobs.txt',
+    'chat_file': 'chats/default.txt',
     'build': '3800-4000',
     'platform': 'android',
     'arch': 'arm64'
